5e9baa60312448c15c44dbb5c7f7025716514a65
Kevin Zhang <mstr.zhang@gmail.com>
2023-05-30 19:03:24 -0400

    mock api call

---
+++ src/@types/Order.ts
@@ -0,0 +1,13 @@
+export interface Order {
+  condition: string,
+  created: string;
+  customer: string;
+  id: string;
+  origin_address: string;
+  photo: string;
+  shipping_address: string;
+  size: string;
+  sku: string;
+  status: string;
+  type: string;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Order } from './@types/Order';
+
+// for demonstration: in a production environment we would be reading from an actual API
+const MOCK_API_URL = 'src/assets/orders.json';
+
 function App() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    fetch(MOCK_API_URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data: { orders: Order[] }) => {
+        setOrders(data.orders);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
-      <div>keviniscool</div>
+      <pre>{JSON.stringify(orders, null, 2)}</pre>
     </>
   );
 }
