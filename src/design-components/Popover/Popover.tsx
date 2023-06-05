5fedd4846e82c50260ebbe6e6ce195f00655fb66
Kevin Zhang <mstr.zhang@gmail.com>
2023-06-05 00:14:21 -0400

    adjust shadow styles

--- src/design-components/Popover/Popover.tsx
+++ src/design-components/Popover/Popover.tsx
@@ -9,7 +9,7 @@ export function Popover({ children, active, activator, title }: PopoverProps) {
   return active ? (
     <div className="group relative inline-block">
       {activator}
-      <div className="absolute right-0 z-50 mt-2 overflow-hidden rounded-md bg-white shadow-xl">
+      <div className="absolute right-0 z-50 mt-2 overflow-hidden rounded-md border border-neutral-200 bg-white shadow-lg shadow-neutral-300">
         <div className="bg-zinc-100 px-2 py-1 font-medium text-zinc-500">
           {title}
         </div>
