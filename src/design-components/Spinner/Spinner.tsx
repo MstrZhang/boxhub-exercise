b7f8b2f81f7f92841ed4657ab8f433010f26718e
Kevin Zhang <mstr.zhang@gmail.com>
2023-06-04 23:51:54 -0400

    adjust spinner styles

--- src/design-components/Spinner/Spinner.tsx
+++ src/design-components/Spinner/Spinner.tsx
@@ -1,5 +1,11 @@
 import { CircleNotch } from '@phosphor-icons/react';
 
 export function Spinner() {
-  return <CircleNotch className="animate-spin" size={84} weight="regular" />;
+  return (
+    <CircleNotch
+      className="animate-spin text-neutral-300"
+      size={84}
+      weight="bold"
+    />
+  );
 }
